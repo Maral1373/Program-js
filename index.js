41f1b66774645cfde9e70ff65d16ffd84635c592
Maral <me.maralerfanian@gmail.com>
2022-08-17 10:16:53 +0200

    second commit

--- index.js
+++ index.js
@@ -2,29 +2,50 @@ import promptSync from "prompt-sync";
 const prompt = promptSync();
 import chalk from "chalk";
 
-let allQuestions = [
-  { question: "2 + 2 ?", answer: 4 },
-  { question: "2 * 12 ?", answer: 24 },
-  { question: "3 - 1 ?", answer: 2 },
-  { question: "12 / 12 ?", answer: 1 },
-  { question: "22 / 2 ?", answer: 11 },
-];
+function game() {
+  const name = prompt(`${chalk.hex("#FFA500")("Hey, What's your name?")}`);
+  console.log(`${chalk.hex("#FFA500")("Hello")} ${chalk.red(name)} 🤩`);
 
-// console.log(allQuestions[0]); // {question: "2 + 2 ?", answer: 4}
-// console.log(allQuestions[1]); // {question: "2 * 12 ?", answer: 24},
-// console.log(allQuestions[4]); // {question: "22 / 2 ?", answer: 11}
+  const age = prompt(`${chalk.hex("#1ABC9C")("How old are you?")}`);
 
-let userScore = 0;
+  console.log(
+    chalk.cyan(`Hello `) +
+      chalk.red(`${name}`) +
+      chalk.magenta(` ${age} `) +
+      chalk.green(`years`) +
+      chalk.yellow(`!😊`)
+  );
 
-for (let i = 0; i < allQuestions.length; i++) {
-  let questionPrompt = prompt(chalk.blue(allQuestions[i].question));
-  if (questionPrompt === allQuestions[i].answer) {
-    userScore++;
+  const resume = prompt(`${chalk.hex("#F8C471")("Do you wanna play? 😇")}`);
+  if (resume !== "yes") {
+    console.log(
+      `${chalk.hex("#5DADE2 ")("Bye")} ${chalk.hex("#7D3C98")(name)} 😕`
+    );
+    return;
   }
+
+  let allQuestions = [
+    { question: "2 + 2 ?", answer: 4 },
+    { question: "2 * 12 ?", answer: 24 },
+    { question: "3 - 1 ?", answer: 2 },
+    { question: "12 / 12 ?", answer: 1 },
+    { question: "22 / 2 ?", answer: 11 },
+  ];
+
+  let userScore = 0;
+
+  for (let i = 0; i < allQuestions.length; i++) {
+    let questionPrompt = prompt(chalk.blue(allQuestions[i].question));
+    if (questionPrompt === allQuestions[i].answer) {
+      userScore++;
+    }
+  }
+
+  console.log(
+    chalk.yellow(
+      `You answered ${chalk.green(userScore)} of 5 questions correctly`
+    )
+  );
 }
 
-console.log(
-  chalk.yellow(
-    `You answered ${chalk.green(userScore)} of 5 questions correctly`
-  )
-);
+game();
